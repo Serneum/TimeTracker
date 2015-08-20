8e9ec313bb16f4cb10fa97df78078c64be34e84f
crr <crr@us.ibm.com>
2015-08-20 15:57:08 -0400

    Add functionality for editing existing tasks. Add method to get preformated due date for simplicity

--- src/main/java/com/example/tasks/Task.java
+++ src/main/java/com/example/tasks/Task.java
@@ -6,6 +6,7 @@ import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 import com.googlecode.objectify.annotation.Parent;
 import com.googlecode.objectify.Key;
+import org.apache.commons.lang.StringUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,7 +14,7 @@ import java.util.Date;
 
 @Entity
 public class Task {
-    private static SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
     @Parent
     Key<TaskUser> user;
@@ -37,6 +38,10 @@ public class Task {
         this.completed = builder.getCompleted();
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getUser() {
         return user.getName();
     }
@@ -49,11 +54,31 @@ public class Task {
         return completed;
     }
 
+    public String getFormattedDueDate() {
+        String result = "";
+        if (dueDate != null) {
+            result = format.format(dueDate);
+        }
+        return result;
+    }
+
     public Date getDueDate() {
         return dueDate;
     }
 
-    private void setDueDate(String dueDate) {
+    public void setDescription(String description) {
+        if (!StringUtils.equals(this.description, description)) {
+            this.description = description;
+        }
+    }
+
+    public void setCompleted(boolean completed) {
+        if (this.completed != completed) {
+            this.completed = completed;
+        }
+    }
+
+    public void setDueDate(String dueDate) {
         try {
             this.dueDate = format.parse(dueDate);
         }
